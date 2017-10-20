4aca2636a51297009bd3dcf70ff39f77f763f656
Chris Castillo <chriscastillo@Chriss-MBP.attlocal.net>
2017-10-19 20:23:07 -0700

    adds permutations

---
+++ permutations.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let results = [];
+
+    let permutations = (current, remaining) => {
+        if(remaining.length === 0) results.push(current.slice());
+        else {
+            for(let i = 0; i < remaining.length; i++) {
+                current.push(remaining[i]);
+                permutations(current, remaining.slice(0, i).concat(remaining.slice(i+1)));
+                current.pop();
+            }
+        }
+    };
+
+    permutations([], nums);
+    return results;
+};
